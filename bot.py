29d5e05273933ffe037b68520df6cf772f71b9ff
Keshoki <53495567+Keshoki@users.noreply.github.com>
2020-09-01 21:08:34 +0000

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,32 @@
+import discord
+from discord.ext import commands
+from random import randint
+
+TOKEN = ""
+
+client = commands.Bot(command_prefix="!!")
+
+membercodes = {}
+
+
+@client.event
+async def on_ready():
+    print("I'm ready!")
+
+@client.event
+async def on_member_join(member):
+    login_code = str(randint(10000000,99999999))
+    membercodes[member.name+"#"+member.discriminator] = str(login_code)
+    await member.send("send this code to #join: "+  login_code)
+
+@client.event
+async def on_message(message):
+    try:
+        if membercodes[str(message.author)] == str(message.content):
+            role = discord.utils.get(message.author.guild.roles, name="slave")
+            await message.author.add_roles(role)
+            del membercodes[str(message.author)]
+    except:
+        pass
+
+client.run(TOKEN)
